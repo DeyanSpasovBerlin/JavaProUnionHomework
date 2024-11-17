f10beda779086fc08a43214152990611eaf2efc2
DeyanSpasovBerlin <pislpe@gmail.com>
2024-11-17 20:45:17 +0100

    Add files via upload

--- Homework_15_11_2024/Task1/MainTask1.java
+++ Homework_15_11_2024/Task1/MainTask1.java
@@ -1,21 +1,18 @@
 package org.example.Homework_15_11_2024.Task1;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.function.Supplier;
 
 import static java.util.Collections.shuffle;
 
 public class MainTask1 {
     public static void main(String[] args) {
-        Reservuar re = new Reservuar();
-        Supplier<String> randomJoke =() ->{
-            return re.getInputList().get(new Random().nextInt(re.getInputList().size()));
-        };
-        System.out.println(randomJoke.get());
+        Start start = new Start();
+        start.startJoke();
     }
 }
-
 
 /*
  Создать Supplier randomJoke, который выдает пользователю случайный анекдот из заготовленного исходного списка анекдотов.
