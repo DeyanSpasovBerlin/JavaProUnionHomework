e7495590289a4ef0ede6bdc223037c56df54be74
DeyanSpasovBerlin <pislpe@gmail.com>
2024-12-15 20:37:57 +0100

    MavenProject

--- src/main/java/Homework_13_12_2024/Main.java
+++ src/main/java/Homework_13_12_2024/Main.java
@@ -4,6 +4,18 @@ import java.util.Iterator;
 
 public class Main {
     public static void main(String[] args) {
+
+        /**************
+        Покрыть тестами все методы класса lesson20241212.CustomDynamicArray (см. репозиторий).
+         по линку:
+         https://github.com/DeyanSpasovBerlin/JavaProUnionHomework/blob/main/src/main/java/Homework_06_12_2024/Task3.java
+         https://github.com/DeyanSpasovBerlin/JavaProUnionHomework/blob/main/src/test/java/Homework_06_12_2024/Task3Test.java
+         *****************************
+         * 1). Написать тест на метод проверки логина и пароля (см. дом. задание от 05-12-2024)
+         по линку:
+         https://github.com/DeyanSpasovBerlin/JavaProUnionHomework/blob/main/src/test/java/Homework_13_12_2024/CustomDynamicArrayTest.java
+        */
+
         CustomDynamicArray cda = new CustomDynamicArray();
         System.out.println("Data: "+cda+" size: "+cda.getSize()+" last index: "+cda.getCount());
         cda.add(1);
